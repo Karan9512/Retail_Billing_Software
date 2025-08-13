b1fe40823930a024493c03674c0bb854468a3745
Karan9512 <Karanchile101@gmail.com>
2025-08-13 10:55:09 +0530

    preoduction ready

--- billingsoftware/src/main/java/com/cdac/billingsoftware/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cdac.billingsoftware.repository;
-
-import com.cdac.billingsoftware.entity.CategoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
-}
